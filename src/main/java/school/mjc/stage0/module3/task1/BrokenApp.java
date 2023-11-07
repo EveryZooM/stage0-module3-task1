c742c9212ecc4e90375e45554d2a1be22876d93e
ZooM <yuhos@mail.ru>
2023-11-07 10:39:05 +0300

    Without import

--- src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
@@ -1,7 +1,5 @@
 package school.mjc.stage0.module3.task1;
 
-import static java.util.logging.Level.INFO;
-
 public class BrokenApp {
     public static void main(String[] args) {
         System.out.println("fixMe");
